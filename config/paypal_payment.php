ebb02d727c8d5ded19ab2434a899f5d9fdedba09
PrinceCBV <pcbv910@gmail.com>
2018-09-18 17:42:47 +0800

    test

--- config/paypal_payment.php
+++ config/paypal_payment.php
@@ -3,13 +3,10 @@
 return array(
 	# Account credentials from developer portal
 	'Account' => array(
-<<<<<<< HEAD
 		'ClientId' => env('PAYPAL_CLIENT_ID', 'AY6JMkm51rObfz6D-nnTJBNe0V-byIra8vZ8v6KW0yzZA7bR9D6yvcjX_ztff6wMo92O3loqvB2PrJhI'),
 		'ClientSecret' => env('PAYPAL_CLIENT_SECRET', 'EO3_dpE-I3hos3UfXyo3W2TRYHcIza-sHCcY475QOBst6vx2WdqC-Vg1Vj8G5Tq61gAV0XAZrlbaC1yy'),
-=======
 		'ClientId' => env('PAYPAL_CLIENT_ID', 'AQ2BWaazy7Nr2gXPvInmHxAaHc57-LanDgb7EOewzJU5JtsdJBeTvNgGTqBjbInSuqs15MIgpsggSYh5'),
 		'ClientSecret' => env('PAYPAL_CLIENT_SECRET', 'EELCgSUZIruhsQJjM1eRbuaqAuMU-IgAaE1rTwQ23pkej-udmPc4ViVnCGcwBPpfSh83fPc-X9h-XJfS'),
->>>>>>> parent of 002b651... Awesome Changes
 	),
 
 	# Connection Information
@@ -23,11 +20,8 @@ return array(
 	'Service' => array(
 		# For integrating with the live endpoint,
 		# change the URL to https://api.paypal.com!
-<<<<<<< HEAD
 		//'EndPoint' => 'https://api.sandbox.paypal.com',
-=======
 		'EndPoint' => 'https://api.sandbox.paypal.com',
->>>>>>> parent of 002b651... Awesome Changes
 	),
 
 
